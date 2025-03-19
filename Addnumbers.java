97ad838452cedcf31d64794682097846b7a124d4
Hira Aslam <153068988+hiraslam@users.noreply.github.com>
2025-03-19 10:45:06 +0500

    Addnumbers.java

--- Addnumbers.java
+++ Addnumbers.java
@@ -3,20 +3,19 @@ import java.util.Scanner;//import to input data
 public class Addnumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// intialize variables
 		int a, b , sum, avg;
 		Scanner Input = new Scanner(System.in);//create object of scanner class
 		System.out.println("Enter the two numbers you want to add:");
 		System.out.println("Number 1:");
-		a= Input.nextInt();
+		a= Input.nextInt(); //get integer input
 		System.out.println("Number2:");
 		b= Input.nextInt();
 		sum=a+b;
 		System.out.println("Sum is:"+ sum);
-		Input.close();//Its not compulsory but it is recommended
 		avg=sum/2;
 		System.out.println("Average of the given numbers:"+ avg);
-		Input.close();
+		Input.close();//Its not compulsory but it is recommended
 	}
 
 }
